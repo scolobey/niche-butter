67c99d0a8420a1d3192182140f3c9e5a481d8059
Ryan Goodwin <minedied@gmail.com>
2022-12-13 19:34:00 -0700

    maybe this will do it

--- pages/api/stripe/create-checkout-session.ts
+++ pages/api/stripe/create-checkout-session.ts
@@ -5,7 +5,7 @@ import Stripe from 'stripe';
 export default async (req: NextApiRequest, res: NextApiResponse) => {
 
   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-    apiVersion: '2020-08-27',
+    apiVersion: '2022-11-15',
   });
 
   // This object will contain the user's data if the user is signed in
